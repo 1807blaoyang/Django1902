1e54ebb79a8108f33ff0e520f9a0fe55485e74a7
1807blaoyang <1143108768@qq.com>
2019-05-31 09:00:02 +0800

    uwsgi.ini

--- demo4/ttsx/views.py
+++ demo4/ttsx/views.py
@@ -11,49 +11,100 @@ def index(request):
 # 注册
 def register(request):
     if request.method =="POST":
-        # 获取表单内容
-        # 错误列表
-        err = []
-        name= request.POST.get("user_name")
+        # 获取表单数据
+        name = request.POST.get("user_name")
         pwd = request.POST.get("pwd")
         cpwd = request.POST.get("cpwd")
         email = request.POST.get("email")
-        print(name,pwd,cpwd,email)
+        allow = request.POST.get("allow")
+        print(name, pwd, cpwd, email)
 
-        # 与数据库对比
+        # 进行数据校验
+        if not all([name,pwd,cpwd,email]):
+            return render(request,"ttsx/register.html",{"err":"请按要求将上述选项填写完整"})
+        if not re.match(r'^[a-z0-9][\w.\-]*@[a-z0-9\-]+(\.[a-z]{2,5}){1,2}$',email):
+            return render(request,"ttsx/register.html",{"err":"邮箱格式不正确！"})
+        if pwd != cpwd:
+            return render(request, "ttsx/register.html", {"err": "密码不一致！"})
+        if allow != "on":
+            return render(request, "ttsx/register.html", {"err": "请同意协议！"})
+
+        # 校验用户名
+        try:
+            username = User.objects.get(name=name)
+        except Exception as e:
+            print(e)
+        username=None
+        if username:
+            return render(request, "ttsx/register.html", {"err": "用户名已存在！"})
+
+        # 校验邮箱
+        try:
+            useremail = User.objects.get(email=email)
+        except Exception as e:
+            print(e)
+        if useremail:
+            return render(request, "ttsx/register.html", {"err": "该邮箱已注册！"})
+
+        # 业务处理
+
         user = User()
         user.name = name
         user.pwd = pwd
         user.cpwd = cpwd
         user.email = email
-        username = User.objects.filter(name=name)
-        useremail = User.objects.filter(email=email)
-        if (name and pwd and cpwd and email):
-            if not username:
-                print("username")
-                if len(pwd) >= 6 and len(pwd) <= 11:
-                    print("pwd")
-                    if pwd == cpwd:
-                        print("cpwd")
-                        if not useremail:
-                            # 保存表单数据
-                            print("useremail")
-                            user.save()
-                            return render(request, "ttsx/login.html")
-                        else:
-                            return render(request, "ttsx/register.html", {"err4": "请重新输入邮箱"})
-
-
-                    else:
-                        return render(request, "ttsx/register.html", {"err3": "两次密码不一致"})
-                else:
-                    return render(request, "ttsx/register.html", {"err2": "密码不符合规定"})
-            else:
-                return render(request, "ttsx/register.html", {"err1": "该用户已注册"})
-        else:
-            return render(request, "ttsx/register.html", {"err": "请按要求将下列选项填写完整"})
+        user.save()
+        # 返回应答,跳转到首页
+        return redirect(reverse("index.html"))
     else:
         return render(request,"ttsx/register.html")
+
+
+
+    #     # 获取表单内容
+    #     # 错误列表
+    #     err = []
+    #     name= request.POST.get("user_name")
+    #     pwd = request.POST.get("pwd")
+    #     cpwd = request.POST.get("cpwd")
+    #     email = request.POST.get("email")
+    #     print(name,pwd,cpwd,email)
+    #
+    #     # 与数据库对比
+    #     user = User()
+    #     user.name = name
+    #     user.pwd = pwd
+    #     user.cpwd = cpwd
+    #     user.email = email
+    #     username = User.objects.filter(name=name)
+    #     useremail = User.objects.filter(email=email)
+    #     if (name and pwd and cpwd and email):
+    #         if not username:
+    #             print("username")
+    #             if len(pwd) >= 6 and len(pwd) <= 11:
+    #                 print("pwd")
+    #                 if pwd == cpwd:
+    #                     print("cpwd")
+    #
+    #                     if not useremail and re.match(r'^[a-z0-9][\w.\-]*@[a-z0-9\-]+(\.[a-z]{2,5}){1,2}$',email):
+    #                         # 保存表单数据
+    #                         print("useremail")
+    #                         user.save()
+    #                         return render(request, "ttsx/login.html")
+    #                     else:
+    #                         return render(request, "ttsx/register.html", {"err4": "请重新输入邮箱"})
+    #
+    #
+    #                 else:
+    #                     return render(request, "ttsx/register.html", {"err3": "两次密码不一致"})
+    #             else:
+    #                 return render(request, "ttsx/register.html", {"err2": "密码不符合规定"})
+    #         else:
+    #             return render(request, "ttsx/register.html", {"err1": "该用户已注册"})
+    #     else:
+    #         return render(request, "ttsx/register.html", {"err": "请按要求将下列选项填写完整"})
+    # else:
+    #     return render(request,"ttsx/register.html")
 
 # 登陆
 def login(request):
